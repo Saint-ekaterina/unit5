2caee0237de037526cbbeea94dce40b882989a3e
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-04-17 14:36:23 +0700

    modification2

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -5,10 +5,8 @@ import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Locale;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class DataGenerator {
     private DataGenerator() {
@@ -19,11 +17,11 @@ public class DataGenerator {
     }
 
     public static String generateCity() {
-        List<String> cities = List.of(
-                "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Казань",
-                "Нижний Новгород", "Челябинск", "Самара", "Омск", "Ростов-на-Дону"
-        );
-        return cities.get(new Random().nextInt(cities.size()));
+        var city = new String[] {
+                "Пермь", "Ижевск", "Екатеринбург", "Казань", "Оренбург",
+                "Самара", "Тольятти", "Новосибирск", "Киров", "Тюмень"
+        };
+        return city[new Random().nextInt(city.length)];
     }
 
     public static String generateName(String locale) {
@@ -33,9 +31,7 @@ public class DataGenerator {
 
     public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
-        return faker.phoneNumber().phoneNumber()
-                .replaceAll("[^0-9+]", "")
-                .replaceFirst("(\\d{1})(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+$1 ($2) $3-$4-$5");
+        return faker.phoneNumber().phoneNumber();
     }
 
     public static class Registration {
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,7 +1,5 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,16 +10,10 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
-
-    @BeforeAll
-    static void setupAll() {
-        Configuration.browserSize = "1280x800";
-        Configuration.timeout = 30000;
-    }
 
     @BeforeEach
     void setup() {
@@ -36,8 +28,6 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-
-        // Заполнение формы первый раз
         $("[data-test-id='city'] input").setValue(validUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(firstMeetingDate);
@@ -45,28 +35,16 @@ class DeliveryTest {
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(byText("Запланировать")).click();
-
-        // Проверка первого уведомления
-        $("[data-test-id='success-notification']")
-                .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
-
-        // Изменение даты
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'].notification .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
-
-        // Проверка уведомления о перепланировании
-        $("[data-test-id='replan-notification']")
-                .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-
-        // Подтверждение перепланирования
+        $("[data-test-id='replan-notification'].notification .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible);
         $(byText("Перепланировать")).click();
-
-        // Проверка финального уведомления
-        $("[data-test-id='success-notification']")
-                .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
+        $("[data-test-id='success-notification'].notification .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(visible);
     }
 }
